798c362c51fe69ee419554439df028d81f7df1bd
Grzegorz Derebecki <grzegorz.derebecki@fdb.pl>
2012-11-12 18:00:47 +0100

    chdir to proper dir

--- ext/libv8/extconf.rb
+++ ext/libv8/extconf.rb
@@ -13,17 +13,24 @@ profile = enable_config('debug') ? 'debug' : 'release'
 
 V8_Version = Libv8::VERSION.gsub(/\.\d$/,'')
 V8_Source = File.expand_path '../../../vendor/v8', __FILE__
+Libv8_Source = File.expand_path '../../..', __FILE__
 
-system "git submodule update --init"
+
+Dir.chdir(Libv8_Source) do
+  system "git submodule update --init"
+end
+
 Dir.chdir(V8_Source) do
   system 'git fetch'
   system "git checkout #{V8_Version} -f"
   system "#{make} dependencies"
 end
 
-system "patch -N -p0 -d vendor/v8 < patches/add-freebsd9-and-freebsd10-to-gyp-GetFlavor.patch"
-system "patch -N -p1 -d vendor/v8 < patches/fPIC-on-x64.patch"
-system "patch -N -p1 -d vendor/v8 < patches/gcc42-on-freebsd.patch" if RUBY_PLATFORM.include?("freebsd") && !system("pkg_info | grep gcc-4")
+Dir.chdir(Libv8_Source) do
+  system "patch -N -p0 -d vendor/v8 < patches/add-freebsd9-and-freebsd10-to-gyp-GetFlavor.patch"
+  system "patch -N -p1 -d vendor/v8 < patches/fPIC-on-x64.patch"
+  system "patch -N -p1 -d vendor/v8 < patches/gcc42-on-freebsd.patch" if RUBY_PLATFORM.include?("freebsd") && !system("pkg_info | grep gcc-4")
+end
 
 Dir.chdir(V8_Source) do
   puts `env CXX=#{compiler} LINK=#{compiler} #{make} #{libv8_arch}.#{profile} GYPFLAGS="-Dhost_arch=#{libv8_arch}"`
